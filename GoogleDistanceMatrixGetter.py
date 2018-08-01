963299e5142dc04f14ca3df19b7697a970a53801
kaihuu <uemura-toshiaki-xh@ynu.jp>
2018-08-01 17:49:29 +0900

    add DBaccess

--- GoogleDistanceMatrixGetter.py
+++ GoogleDistanceMatrixGetter.py
@@ -5,37 +5,29 @@ import PolylineDecoder as dc
 import os
 import time
 import threading
-
-#DBアクセス処理
-
-latitude = 133.1
-longitude = 111.2
-print([latitude, longitude])
-
-#API前準備
-api_key = os.environ["GOOGLE_MAP_API_KEY"]
-gmaps = googlemaps.Client(key=api_key)
-
-#distance matrix呼び出しRequest処理
-now = datetime.now()
-
-
-#JSONパース
-
-
-#DBインサート
-
+import sched
 
 def schedule(interval, f, wait=False):
+    minterval = 1
+    while True:
+        now = time.time()
+        nowd = datetime.now()
+        if nowd.minute == 0 and nowd.second == 0:
+            break
+        next_time = ((now - time.time()) % minterval) or minterval
+        time.sleep(next_time)
+
     base_time = time.time()
     next_time = 0
     while True:
         t = threading.Thread(target=f)
         t.start()
-        if wait:
-            t.join()
-    next_time = ((base_time - time.time()) % interval) or interval
-    time.sleep(next_time)
+        next_time = ((base_time - time.time()) % interval) or interval
+        time.sleep(next_time)
+
+def Inserter():
+    print(datetime.now())
+
 
 def getMatrix(now, startLatitude, startLongitude, endLatitude, endLongitude):
     matrix_result = gmaps.distance_matrix([startLatitude, startLongitude],
@@ -46,3 +38,13 @@ def getMatrix(now, startLatitude, startLongitude, endLatitude, endLongitude):
                                         )
 
     return matrix_result
+
+#DBアクセス
+
+#API前準備
+api_key = os.environ["GOOGLE_MAP_API_KEY"]
+gmaps = googlemaps.Client(key=api_key)
+
+#distance matrix呼び出しRequest処理,JSONパース、DBインサート
+schedule(3600, Inserter)
+
